677a6d74b3769f6c50a5f9517d69e50cfa0c0874
Vikk_Moor <victimofuroboros@gmail.com>
2022-11-29 04:24:50 +0500

    1stReady

---
+++ 1st/Program.cs
@@ -0,0 +1,30 @@
+﻿// Input - array (99 < i < 1000), output - how much even numbers.
+void FillArray(int[] arr1)
+{
+    int len1 = arr1.Length;
+    int pos1 = 0;
+    while (pos1 < len1)
+    {
+        arr1[pos1] = new Random().Next(100, 1000);
+        pos1++;
+    }
+}
+void PrintArray(int[] arr2)
+{
+    int len2 = arr2.Length;
+    int pos2 = 0;
+    while (pos2 < len2)
+    {
+        Console.Write($"{arr2[pos2]} ");
+        pos2++;
+    }
+}
+Console.Write("Enter array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < size; i++) if (array[i] % 2 == 0) count++;
+Console.Write(count);
